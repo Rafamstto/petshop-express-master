7e4d7ba019a40a2c65500acaa5d4dcbdf543a839
rafaelmstto <rafaelmstto@mail.comm>
2022-02-09 22:06:22 -0300

    aula 09-02

---
+++ routes/cadastro.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const cadastroController = require('../controllers/cadastroController');
+const router = express.Router();
+
+router.get('/', cadastroController.index);
+
+module.exports = router;
